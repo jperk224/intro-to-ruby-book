652222fbfbf01328eaccb1a3e663b90fdb03c300
Jeff Perkinson <jmperkinson@gmail.com>
2020-08-03 09:09:48 -0400

    Add hashes exercises.

---
+++ end-of-section-exercises/hashes/labled_numbers.rb
@@ -0,0 +1,22 @@
+numbers = {
+  high:   100,
+  medium: 50,
+  low:    10
+}
+
+# numbers.each do |key, value|
+#   puts "A #{key} number is #{value}."
+# end
+
+# half_numbers = numbers.map do |key, value|
+#   value / 2
+# end
+
+# p half_numbers
+
+low_numbers = numbers.select! { |key, value| value < 25 }
+
+low_numbers[:new] = 376
+
+p low_numbers
+p numbers
