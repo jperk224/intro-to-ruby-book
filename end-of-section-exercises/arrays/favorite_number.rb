7cf46c79ba9af2be485460f31369ad227191032b
Jeff Perkinson <jmperkinson@gmail.com>
2020-08-03 08:30:27 -0400

    Add arrays exercises.

---
+++ end-of-section-exercises/arrays/favorite_number.rb
@@ -0,0 +1,13 @@
+# start_array = ['Dave', 7, 'Miranda', 3, 'Jason', 11]
+
+# dave = [start_array[0], start_array[1]]
+# miranda = [start_array[2], start_array[3]]
+# jason = [start_array[4], start_array[5]]
+
+# nested_array = [dave, miranda, jason]
+
+# p nested_array
+
+favorites = [['Dave', 7], ['Miranda', 3], ['Jason', 11]]
+
+p favorites.flatten
---
+++ end-of-section-exercises/arrays/pets.rb
@@ -0,0 +1,17 @@
+pets = ['cat', 'dog', 'fish', 'lizard']
+my_pets = pets[2..3]
+my_pets.pop
+
+# my_pet = pets[2]
+
+# puts "I have a pet #{my_pet}!"
+
+# my_pets = pets[2,2]
+
+# my_pets.pop()
+
+# puts "I have a pet #{my_pets.join('')}!"
+
+my_pets << pets[1]
+
+puts "I have a pet #{my_pets[0]} and a pet #{my_pets[1]}!"
